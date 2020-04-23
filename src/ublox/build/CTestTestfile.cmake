028d4e586d6556c7ab42890bdb1ad8cc98e84e9c
rnd <lineardept@mgmail.com>
2020-04-23 14:15:42 +0900

    ins branch

---
+++ src/ublox/build/CTestTestfile.cmake
@@ -0,0 +1,11 @@
+# CMake generated Testfile for 
+# Source directory: /home/rnd/ros/ublox_ws/src
+# Build directory: /home/rnd/ros/ublox_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ublox/ublox")
+subdirs("ublox/ublox_serialization")
+subdirs("ublox/ublox_msgs")
+subdirs("ublox/ublox_gps")
