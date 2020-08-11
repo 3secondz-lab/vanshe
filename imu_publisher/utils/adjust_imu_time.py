a378c4c907e78d475315468c9e9bba251482a610
Jinsun <zzangjinsun@gmail.com>
2020-08-11 15:23:47 +0900

    Automatic output file naming

--- imu_publisher/utils/adjust_imu_time.py
+++ imu_publisher/utils/adjust_imu_time.py
@@ -9,7 +9,6 @@ import argparse
 
 parser = argparse.ArgumentParser(description='Rosbag IMU time adjustor')
 parser.add_argument('--input', type=str, required=True, help='Path to input bag')
-parser.add_argument('--output', type=str, required=True, help='Path to output bag')
 parser.add_argument('--t', type=float, required=False, default=0.7, help='Adjust time in secs')
 parser.add_argument('--topic', type=str, required=False, default='/imu/data', help='Topic name')
 
@@ -18,7 +17,10 @@ args = parser.parse_args()
 
 if __name__ == '__main__':
     bag_in = rosbag.Bag(args.input, 'r')
-    bag_out = rosbag.Bag(args.output, 'w')
+    path_out = args.input[:-4] + '_adj.bag'
+    bag_out = rosbag.Bag(path_out, 'w')
+
+    print('{} -> {}'.format(args.input, path_out))
 
     t_adj_ns = args.t*1e9
 
