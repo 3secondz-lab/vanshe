e1da643a4dbc338f7b0191283cb42fe95aeef876
Jinsun <zzangjinsun@gmail.com>
2020-08-18 13:31:22 +0900

    imu publisher based on streaming

--- imu_publisher/src/imu_publisher.cpp
+++ imu_publisher/src/imu_publisher.cpp
@@ -16,8 +16,9 @@
 #include <ros/ros.h>
 #include <sensor_msgs/Imu.h>
 
-#define BUF_SIZE 512
-#define NUM_DATA 8
+#define BUF_SIZE 1024
+#define NUM_DATA 9
+#define G_VALUE 9.80665
 
 using namespace std;
 
@@ -35,10 +36,26 @@ void requestStopHandler(int s){
 }
 
 string read_line_check(int fd, char* buf, int buf_size){
+    bool found = false;
+
+    while(!found){
+        int cnt = 0;
+        while(cnt <= 0){
+            cnt = read(fd, buf, 1);
+        }
+
+        if(*buf == '$'){
+            found = true;
+        }
+    }
+
     int num_space = 0;
 
-    for(int k = 0; k < buf_size; k++){
-        int cnt = read(fd, buf+k, 1);
+    for(int k=1; k < buf_size; k++){
+        int cnt = 0;
+        while(cnt <= 0){
+            cnt = read(fd, buf+k, 1);
+        }
 
         if(*(buf+k) == ' '){
             num_space++;
@@ -49,22 +66,26 @@ string read_line_check(int fd, char* buf, int buf_size){
                 return string();
             }
 
-            *(buf+k) = '\0';
+            // *(buf+k) = '\0';
             string out = string(buf);
             *buf = '\0';
             return out;
         }
     }
 
+    *buf = '\0';
     return string();
 }
 
 sensor_msgs::Imu gen_msg_from_data(char data[][BUF_SIZE], uint64_t tOffset){
     sensor_msgs::Imu imu;
 
+    // Expected data format :
+    // [$ Timestamp AccX AccY AccZ Temp GyrX GyrY GyrZ]
+
     // Sensor time : us -> ns
     // Ros time : ns
-    uint64_t tSensor = 1000 * (uint64_t) atoi(data[0]);
+    uint64_t tSensor = 1000 * (uint64_t) atoi(data[1]);
     uint64_t t = tSensor + tOffset;
 
     imu.header.stamp.fromNSec(t);
@@ -78,9 +99,9 @@ sensor_msgs::Imu gen_msg_from_data(char data[][BUF_SIZE], uint64_t tOffset){
     imu.orientation_covariance = {-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
 
     // Check Acc
-    string ax = string(data[1]);
-    string ay = string(data[2]);
-    string az = string(data[3]);
+    string ax = string(data[2]);
+    string ay = string(data[3]);
+    string az = string(data[4]);
 
     if((ax == "NA") || (ay == "NA") || (az == "NA")){
         imu.linear_acceleration.x = 0.0;
@@ -117,9 +138,9 @@ sensor_msgs::Imu gen_msg_from_data(char data[][BUF_SIZE], uint64_t tOffset){
     // string temp = string(data[4]);
 
     // Check Gyr
-    string gx = string(data[5]);
-    string gy = string(data[6]);
-    string gz = string(data[7]);
+    string gx = string(data[6]);
+    string gy = string(data[7]);
+    string gz = string(data[8]);
 
     if((gx == "NA") || (gy == "NA") || (gz == "NA")){
         imu.angular_velocity.x = 0.0;
@@ -169,12 +190,14 @@ int main(int argc, char** argv){
     nh.param("acc_fsr", acc_fsr, 2.0);
     nh.param("gyr_fsr", gyr_fsr, 125.0);
 
-    cout << "freq_imu : " << freq_imu << endl;
-    cout << "topic_imu : " << topic_imu << endl;
-    cout << "dev_imu : " << dev_imu << endl;
+    cout << "freq_imu           : " << freq_imu << endl;
+    cout << "topic_imu          : " << topic_imu << endl;
+    cout << "dev_imu            : " << dev_imu << endl;
 
-    cout << "acc_fsr : " << acc_fsr << endl;
-    cout << "gyr_fsr : " << gyr_fsr << endl;
+    cout << "acc_fsr (g)        : " << acc_fsr << endl;
+    acc_fsr = acc_fsr*G_VALUE;
+    cout << "acc_fsr (m/s^2)    : " << acc_fsr << endl;
+    cout << "gyr_fsr            : " << gyr_fsr << endl;
 
     // Connect to IMU device
     int fd;
@@ -183,6 +206,7 @@ int main(int argc, char** argv){
         cout << "Failed to open " << dev_imu << endl;
         return -1;
     }
+    cout << "Connected to " << dev_imu << endl;
 
     struct termios newtio;
     memset(&newtio, 0, sizeof(newtio));
@@ -207,14 +231,13 @@ int main(int argc, char** argv){
     // Timestamp synchronization
     uint64_t tOffset = 0;
 
+    cout << "Try time synchronization..." << endl;
+
     bool isInitialized = false;
     while(!isInitialized && nh.ok() && !request_stop){
         try{
-            // Get line from serial port
-            // NOTE : Sending 's' -> Get output
             ros::Time tRos = ros::Time::now();
 
-            write(fd, "s\n", 1);
             str_line = read_line_check(fd, buf, BUF_SIZE);
 
             if(str_line.empty()){
@@ -222,11 +245,11 @@ int main(int argc, char** argv){
             }
 
             // Expected data format :
-            // [Timestamp AccX AccY AccZ Temp GyrX GyrY GyrZ]
+            // [$ Timestamp AccX AccY AccZ Temp GyrX GyrY GyrZ]
             char vals[NUM_DATA][BUF_SIZE] = {0};
-            int num = sscanf(str_line.c_str(), "%s %s %s %s %s %s %s %s",
+            int num = sscanf(str_line.c_str(), "%s %s %s %s %s %s %s %s %s",
                                 vals[0], vals[1], vals[2], vals[3],
-                                vals[4], vals[5], vals[6], vals[7]);
+                                vals[4], vals[5], vals[6], vals[7], vals[8]);
 
             if(num != NUM_DATA){
                 continue;
@@ -234,7 +257,7 @@ int main(int argc, char** argv){
 
             // Sensor time : us -> ns
             // Ros time : ns
-            uint64_t tSensor = 1000 * (uint64_t) atoi(vals[0]);
+            uint64_t tSensor = 1000 * (uint64_t) atoi(vals[1]);
             uint64_t tRosNs = tRos.toNSec();
             tOffset = tRosNs - tSensor;
 
@@ -245,14 +268,16 @@ int main(int argc, char** argv){
             cout << "Exception occured : " << e.what() << endl;
         }
         
+        if(isInitialized){
+            cout << "Time synchronization is finished." << endl;
+        }else{
+            cout << "Try time synchronization again..." << endl;
+        }
         r.sleep();
     }
 
     while(nh.ok() && !request_stop){
         try{
-            // Get line from serial port
-            // NOTE : Sending 's' -> Get output
-            write(fd, "s\n", 1);
             str_line = read_line_check(fd, buf, BUF_SIZE);
 
             if(str_line.empty()){
@@ -260,11 +285,11 @@ int main(int argc, char** argv){
             }
 
             // Expected data format :
-            // [Timestamp AccX AccY AccZ Temp GyrX GyrY GyrZ]
+            // [$ Timestamp AccX AccY AccZ Temp GyrX GyrY GyrZ]
             char vals[NUM_DATA][BUF_SIZE] = {0};
-            int num = sscanf(str_line.c_str(), "%s %s %s %s %s %s %s %s",
+            int num = sscanf(str_line.c_str(), "%s %s %s %s %s %s %s %s %s",
                                 vals[0], vals[1], vals[2], vals[3],
-                                vals[4], vals[5], vals[6], vals[7]);
+                                vals[4], vals[5], vals[6], vals[7], vals[8]);
 
             if(num != NUM_DATA){
                 continue;
--- imu_publisher/utils/DK42605_Firmware/sources/examples/example-raw-data-registers/interrupt_rx.h
+++
@@ -1,9 +0,0 @@
-﻿#ifndef INTERRUPT_RX_H_
-#define INTERRUPT_RX_H_
-
-static volatile int irq_from_rx = 0;
-
-
-
-
-#endif /* INTERRUPT_RX_H_ */
